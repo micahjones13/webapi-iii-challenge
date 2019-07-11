48869e6bc457732cb640b26b9527744841786741
micahjones13 <micahjones13@gmail.com>
2019-07-11 13:24:52 -0600

    Added .env, port, and start script in order to deploy

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // code away!
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const server = require('./server.js');
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
